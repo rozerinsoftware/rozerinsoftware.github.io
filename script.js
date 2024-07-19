e666183ad9cc9f254d68feafc41e99c94eccde44
Rozerin <byztrozerin@gmail.com>
2024-07-19 13:58:29 +0300

    Music added İmage added

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.getElementById('myButton').addEventListener('click', function() {
     alert('Button clicked!');
   console.log("button clicked")
 });
+
+document.getElementById('playButton').addEventListener('click', function() {
+  document.getElementById('audio').play();
+});
+
+document.getElementById('pauseButton').addEventListener('click', function() {
+  document.getElementById('audio').pause();
+});
